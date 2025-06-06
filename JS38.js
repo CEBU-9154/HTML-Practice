7824377abff8a816a0605f63c3f62282f8847d72
CEBU-9154 <unida.chrisha@gmail.com>
2025-06-06 22:07:13 +0800

    Update JS38.js

--- JS38.js
+++ JS38.js
@@ -1,8 +1,6 @@
 function palindrome(myString){
     var input = myString.replace(/[^A-Z0-9]/ig, "").toLowerCase();
-
     var reversedInput = input.split('').reverse().join('');
-
     if (input === reversedInput) {
         document.write("<div>" + myString + " is a palindrome <div>")
     }
