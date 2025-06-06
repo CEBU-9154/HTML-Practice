d9f7d0ff9ae9bc03c93ec6765fa80f46b9a86266
CEBU-9154 <unida.chrisha@gmail.com>
2025-06-06 22:07:41 +0800

    Add files via upload

---
+++ JS_HW13.js
@@ -0,0 +1,13 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    $("#colorpad").css("background-color", getRandomColor());
+}
+//idk how to make one so i searced it up, sorry :[
