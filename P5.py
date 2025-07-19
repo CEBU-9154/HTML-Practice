23ed851df192efbdf1e54591655dfc8dabbd67c0
CEBU-9154 <unida.chrisha@gmail.com>
2025-07-19 21:17:42 +0800

    Add files via upload

---
+++ P5.py
@@ -0,0 +1,20 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name: ", name)
+print("Data type of Name: ", type(name))
+print("Age: ", age)
+print("Data type of Age: ", type(age))
+print("Is a student: ", is_student)
+print("Data type of is_student: ", type(is_student))
+print("Weight: ", weight)
+print("Data type of Weight: ", type(weight))
+
+print("\n ...")
+age=str(age)
+print(age)
+print("Data type of Age: ", type(age))
+weight=int(weight)
+print(weight)
+print("Data type of Weight: ", type(weight))
