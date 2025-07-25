75d332549747352e19611b8c250f64f17bf03ae4
CEBU-9154 <unida.chrisha@gmail.com>
2025-07-25 21:14:35 +0800

    Add files via upload

---
+++ P10.py
@@ -0,0 +1,6 @@
+import datetime
+import calendar
+current_time=datetime.datetime.now()
+print("Time now at greenwich meridian is: ", end="")
+print(current_time)
+print("\n", calendar.calendar(2021))
