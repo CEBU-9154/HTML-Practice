7e0940dbe5951f338e0f65f79202eba792b4a68d
CEBU-9154 <unida.chrisha@gmail.com>
2025-08-08 21:18:49 +0800

    Add files via upload

---
+++ P27.py
@@ -0,0 +1,17 @@
+setx={"green", "blue"}
+sety={"blue", "yellow"}
+print("Original set elements: ")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets: ")
+setz=setx&sety
+print(setz)
+
+setc1={"green", "blue"}
+setc2={"blue", "yellow"}
+print("Original set elements: ")
+print(setc1)
+print(setc2)
+setc=setc1|setc2
+print("\nUnion of above sets: ")
+print(setc)
