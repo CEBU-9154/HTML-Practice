ec5a7d861f1c6882f1d3d32fd7ad97bdd8f5ea41
CEBU-9154 <unida.chrisha@gmail.com>
2025-09-05 21:15:27 +0800

    Add files via upload

---
+++ P46.py
@@ -0,0 +1,9 @@
+def printnum(n):
+    iteration=0
+    print("The number entered by user is ", n)
+    iteration+=1
+    print("Total iterations done by the code is ", iteration, "\n")
+
+printnum(10)
+printnum(20)
+print("\n With any 'n', the time taken by our code won't change.")
