fdc56957b89a2b5c9aed13e0c999c72ade991a88
CEBU-9154 <unida.chrisha@gmail.com>
2025-09-05 21:31:47 +0800

    Add files via upload

---
+++ P49.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+arr=[1,3,5,7,9,11,13,15,17,19,21]
+target=13
+
+index=binary_search(arr, target)
+print(index)
