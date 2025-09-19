2b946bae50b4aee06204de9f34b82519edd5bc00
CEBU-9154 <unida.chrisha@gmail.com>
2025-09-19 21:16:59 +0800

    Add files via upload

---
+++ P64.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+two_digit_primes=[]
+for num in range(10,100):
+    if is_prime(num):
+        two_digit_primes.append(num)
+
+print("Two digit prime numbers are:")
+print(two_digit_primes)
