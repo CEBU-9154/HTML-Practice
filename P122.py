fac1045e249ce63a5546192fe7ff3aee1dbf1408
CEBU-9154 <unida.chrisha@gmail.com>
2025-11-15 21:26:20 +0800

    Add files via upload

---
+++ P122.py
@@ -0,0 +1,20 @@
+def sort(arr):
+    low,mid,high=0,0,len(arr)-1
+
+    while mid <= high:
+        if arr[mid]==0:
+            arr[low], arr[mid]=arr[mid], arr[low]
+            low+=1
+            mid+=1
+        elif arr[mid] ==1:
+            mid+1
+        else:
+            arr[high], arr[mid]=arr[mid], arr[high]
+            high-=1
+    return arr
+
+n=int(input("Enter number of elements: "))
+arr=list(map(int, input("Enter the elements (0,1,2) only: ").split()))
+
+sorted_arr=sort(arr)
+print("Sorted array: ", *sorted_arr)
