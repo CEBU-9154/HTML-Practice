b0004275cbea9ae94190375968b71bb4affa5d66
CEBU-9154 <unida.chrisha@gmail.com>
2025-12-06 21:30:40 +0800

    Add files via upload

---
+++ P136.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df=pd.read_csv("USA_Housing.csv")
+print(df.head(10))
+print(df.info())
+print(df.describe())
+print(df.columns)
+#print(sns.heatmap(df.corr(), annot=True))
