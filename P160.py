314119f6a5303c9a78e1309ef4f7cf18407476df
CEBU-9154 <unida.chrisha@gmail.com>
2026-02-28 21:30:48 +0800

    Add files via upload

---
+++ P160.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+months=["Jan","Feb","Mar","Apr","May","Jun"]
+marks=[65,70,75,72,80,85]
+plt.plot(months,marks)
+plt.title("Student Marks Over 6 Months")
+plt.xlabel("Months")
+plt.ylabel("Marks")
+
+plt.show()
