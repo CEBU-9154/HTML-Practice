2276ed1ff119710e20b4cbe355d71e251bb5b73c
CEBU-9154 <unida.chrisha@gmail.com>
2026-03-13 21:58:18 +0800

    Add files via upload

---
+++ P162.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+cats=['A','B','C','D']
+vals=[10,15,7,12]
+plt.bar(cats,vals)
+plt.title("Bar Chart Example")
+plt.xlabel("Categories")
+plt.ylabel("Values")
+plt.show()
